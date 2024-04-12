32bb4c5c6c16bd80bbe4ee19cf4a40cff89b126d
Bernardo Lopez Rosso <bernardolr17@gmail.com>
2024-04-11 22:31:14 -0600

    added script

---
+++ script.js
@@ -0,0 +1,36 @@
+// Random Movie director's names generator
+
+const directors = [
+    "Christopher Nolan",
+    "Steven Spielberg",
+    "Quentin Tarantino",
+    "Martin Scorsese",
+    "Alfred Hitchcock"
+];
+
+const movies = [
+    "Inception",
+    "The Dark Knight",
+    "Interstellar",
+    "Jurassic Park",
+    "Schindler's List",
+    "Pulp Fiction",
+    "Django Unchained",
+    "Goodfellas",
+    "Taxi Driver",
+    "Psycho"
+];
+
+const moviesAndDirectors = [directors, movies];
+
+console.log(moviesAndDirectors);
+
+// const arr = [0,1,2,3,4];
+
+// function logNumber(number){
+//     console.log(number);
+// }
+
+// arr.forEach(logNumber);
+
+
