079845ad13216aa2bbda3d069fefdcded82328ec
Bernardo Lopez Rosso <bernardolr17@gmail.com>
2024-04-11 22:54:19 -0600

    added function for random movie info

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Random Movie director's names generator
+// Random Movie generator
 
 const directors = [
     "Christopher Nolan",
@@ -21,9 +21,39 @@ const movies = [
     "Psycho"
 ];
 
-const moviesAndDirectors = [directors, movies];
+const years = [
+    1985,
+    2001,
+    2017,
+    2022,
+    2009,
+    1999,
+    2014,
+    2019,
+    2008,
+    2020
+];
 
-console.log(moviesAndDirectors);
+//const moviesAndDirectors = [directors, movies, years];
+
+function randomMovie(directors,movies,years) { 
+    const randomDirector = directors[Math.floor(Math.random() * directors.length)];
+    const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+    const randomYear = years[Math.floor(Math.random() * years.length)];
+
+
+    return randomDirector + " " + randomMovie + " " + randomYear;
+    //console.log(randomSelection);
+
+}
+
+//const randomSelection = randomMovie(directors);
+
+
+console.log(randomMovie(directors,movies,years));
+
+
+
 
 // const arr = [0,1,2,3,4];
 
